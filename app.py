ce9e4534141f82870f8d253b0a4074d869269e7e
yulianendeshaw <yendesha@terpmail.umd.edu>
2023-03-30 14:17:11 -0400

    edited app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Yulian Endeshaw! I am adding my first code change'
-@app.route('/favorite-course')
+@app.route('favorite-course')
 def favorite_course():  # put application's code here
     print('Your favorite course name:' + request.args.get('subject'))
     print('Your favorite course number:' + request.args.get('course_number'))
     return render_template('favorite-course.html')
 
-@app.route('/contact', methods=['GET', 'POST'])
+@app.route('contact', methods=['GET', 'POST'])
 def contact():  # put application's code here
     if request.method == 'POST':
         return render_template('contact.html', form_submitted=True)
